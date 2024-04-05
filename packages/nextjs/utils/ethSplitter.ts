502bb054e73dacd88c894c726f3b82ab70dd3c89
avelous <avelousujiri@gmail.com>
2024-04-05 22:02:17 +0100

    configured caching for persistence of split data when reloading and switching split types

--- packages/nextjs/utils/ethSplitter.ts
+++ packages/nextjs/utils/ethSplitter.ts
@@ -1,8 +1,9 @@
-const STORAGE_KEY = "ES_CONTACTS_SK";
+const CONTACTS_STORAGE_KEY = "ES_CONTACTS_SK";
+const CACHE_STORAGE_KEY = "ES_CACHE_SK";
 
 export const saveContacts = (contacts: string[]) => {
   if (typeof window != "undefined" && window != null) {
-    const savedContacts = window.localStorage.getItem(STORAGE_KEY);
+    const savedContacts = window.localStorage.getItem(CONTACTS_STORAGE_KEY);
 
     if (savedContacts) {
       const contactsObj = JSON.parse(savedContacts);
@@ -11,14 +12,70 @@ export const saveContacts = (contacts: string[]) => {
           contactsObj.push(contact);
         }
       });
-      window.localStorage.setItem(STORAGE_KEY, JSON.stringify(contactsObj));
-    } else window.localStorage.setItem(STORAGE_KEY, JSON.stringify(contacts));
+      window.localStorage.setItem(CONTACTS_STORAGE_KEY, JSON.stringify(contactsObj));
+    } else window.localStorage.setItem(CONTACTS_STORAGE_KEY, JSON.stringify(contacts));
   }
 };
 
 export const loadContacts = () => {
   if (typeof window != "undefined" && window != null) {
-    const contacts = window.localStorage.getItem(STORAGE_KEY);
+    const contacts = window.localStorage.getItem(CONTACTS_STORAGE_KEY);
     if (contacts) return JSON.parse(contacts);
   } else return null;
+};
+
+export const loadCache = () => {
+  if (typeof window != "undefined" && window != null) {
+    const cachedString = localStorage.getItem(CACHE_STORAGE_KEY);
+    const now = new Date().getTime();
+    if (!cachedString) {
+      const item = {
+        wallets: [],
+        amounts: [],
+        amount: "",
+        expiry: now + 30 * 60 * 1000,
+      };
+      localStorage.setItem(CACHE_STORAGE_KEY, JSON.stringify(item));
+      return null;
+    }
+    const cacheData = JSON.parse(cachedString);
+    if (now > cacheData.expiry) {
+      localStorage.removeItem(cacheData);
+      return null;
+    }
+    return { wallets: cacheData.wallets, amounts: cacheData.amounts, amount: cacheData.amount };
+  }
+};
+
+export const updateCacheWallets = (wallets: string[]) => {
+  if (typeof window != "undefined" && window != null) {
+    const cachedString = localStorage.getItem(CACHE_STORAGE_KEY);
+    if (cachedString) {
+      const cacheData = JSON.parse(cachedString);
+      cacheData.wallets = wallets;
+      localStorage.setItem(CACHE_STORAGE_KEY, JSON.stringify(cacheData));
+    }
+  }
+};
+
+export const updateCacheAmounts = (amounts: string[]) => {
+  if (typeof window != "undefined" && window != null) {
+    const cachedString = localStorage.getItem(CACHE_STORAGE_KEY);
+    if (cachedString) {
+      const cacheData = JSON.parse(cachedString);
+      cacheData.amounts = amounts;
+      localStorage.setItem(CACHE_STORAGE_KEY, JSON.stringify(cacheData));
+    }
+  }
+};
+
+export const updateCacheAmount = (amount: string) => {
+  if (typeof window != "undefined" && window != null) {
+    const cachedString = localStorage.getItem(CACHE_STORAGE_KEY);
+    if (cachedString) {
+      const cacheData = JSON.parse(cachedString);
+      cacheData.amount = amount;
+      localStorage.setItem(CACHE_STORAGE_KEY, JSON.stringify(cacheData));
+    }
+  }
 };
