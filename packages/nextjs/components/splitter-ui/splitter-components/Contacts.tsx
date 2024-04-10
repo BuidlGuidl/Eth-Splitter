64c3c097e09394eea54ee2d0183a52a0d926574c
Avelous Ujiri <86206128+Avelous@users.noreply.github.com>
2024-04-10 16:02:38 +0100

    merge Staging (#74)
    * Added filtration of invalid addresses from contacts list & edited unequal split ui for smaller screens (#73)

--- packages/nextjs/components/splitter-ui/splitter-components/Contacts.tsx
+++ packages/nextjs/components/splitter-ui/splitter-components/Contacts.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { PlusIcon } from "@heroicons/react/24/solid";
+import { isAddress } from "viem";
 import { Address } from "~~/components/scaffold-eth";
 import { loadContacts } from "~~/utils/ethSplitter";
 
@@ -8,41 +8,42 @@ const Contacts = ({ setWallets, wallets }: { setWallets: Dispatch<SetStateAction
 
   useEffect(() => {
     const loadedContacts = loadContacts();
-    const contacts = loadedContacts?.filter((contact: string) => !wallets.includes(contact));
+    const contacts = loadedContacts?.filter((contact: string) => !wallets.includes(contact) && isAddress(contact));
     setContacts(contacts);
   }, [wallets]);
 
   return (
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn m-1 btn-primary btn-sm">
-        Contacts
-      </div>
+    <div>
+      <details className="dropdown dropdown-end">
+        <summary tabIndex={0} role="button" className="btn m-1 btn-primary btn-sm">
+          Contacts
+        </summary>
 
-      <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box max-h-72 ">
-        <div className="overflow-y-scroll">
-          {contacts?.length > 0 ? (
-            contacts?.map((contact: string) => (
-              <li key={contact}>
-                <a
-                  className="flex justify-between flex-row tooltip"
-                  data-tip="Add to recipients"
-                  onClick={() => {
-                    setWallets(prevWallets => {
-                      const filteredWallets = prevWallets.filter(wallet => wallet !== "");
-                      return [...filteredWallets, contact];
-                    });
-                  }}
-                >
-                  <Address hideBlockie={true} hideCopyIcon={true} disableAddressLink={true} address={contact} />
-                  <PlusIcon className="w-1/6" />
-                </a>
-              </li>
-            ))
-          ) : (
-            <div className="p-2">Empty List</div>
-          )}
-        </div>
-      </ul>
+        <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box max-h-72 ">
+          <div className="overflow-y-scroll">
+            {contacts?.length > 0 ? (
+              contacts?.map((contact: string, index) => (
+                <li key={contact}>
+                  <a
+                    className={`flex justify-between flex-row tooltip` + (index === 0 ? ` tooltip-bottom` : ``)}
+                    data-tip="Add to recipients"
+                    onClick={() => {
+                      setWallets(prevWallets => {
+                        const filteredWallets = prevWallets.filter(wallet => wallet !== "");
+                        return [...filteredWallets, contact];
+                      });
+                    }}
+                  >
+                    <Address hideBlockie={true} hideCopyIcon={true} disableAddressLink={true} address={contact} />
+                  </a>
+                </li>
+              ))
+            ) : (
+              <div className="p-2">Empty List</div>
+            )}
+          </div>
+        </ul>
+      </details>
     </div>
   );
 };
